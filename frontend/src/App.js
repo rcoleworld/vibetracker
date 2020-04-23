3c1758d5009207ce0d77ff4239011606506f9219
Reginald Thomas <reginaldcolethomas@gmail.com>
2020-04-23 04:42:36 -0500

    added pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, withRouter, Redirect} from "react-router-dom";
 import Login from './components/pages/Login';
+import Home from './components/pages/Home';
+import MyVibes from './components/pages/MyVibes';
+import NearbyVibes from './components/pages/NearbyVibes';
+import TrackVibe from './components/pages/TrackVibe';
 
 import './App.css';
 
@@ -10,6 +14,11 @@ function App() {
       <Router>
         <Switch>
           <Route exact path='/' component={withRouter(Login)}/>
+          <Route exact path='/home' component={withRouter(Home)}/>
+          <Route exact path='/vibes' component={withRouter(MyVibes)}/>
+          <Route exact path='/nearby' component={withRouter(NearbyVibes)}/>
+          <Route exact path='/track' component={withRouter(TrackVibe)}/>
+
         </Switch>
       </Router>
     </div>
