44349403da436d517f7f4078c8d2bb450547e7bf
Reginald Thomas <reginaldcolethomas@gmail.com>
2020-04-30 09:04:35 -0500

    location validation error quick fix

--- api/models/locationModel.js
+++ api/models/locationModel.js
@@ -6,8 +6,8 @@ const LocationSchema = new Schema({
     username: {type: String, required: true},
     title: {type: String},
     description: { type: String },
-    latitude: {type: Number, required: true},
-    longitude: {type: Number, required: true}
+    latitude: {type: Number},
+    longitude: {type: Number}
 })
 
 module.exports = Model('Location', LocationSchema)
